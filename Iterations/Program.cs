fa1eb708c8bf608ea6e0b5c2b1da1c0180473668
monomarine <gamedev409@yandex.ru>
2024-01-26 14:40:35 +0500

    Добавьте файлы проекта.

---
+++ Iterations/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Iterations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //простой пример использования цикла с с предусловием while
+            //выводятся все четные числа от 0 до 100 в консоль
+            //но при значении 50, цикл останавливается 
+
+            int i = 0;
+            while(i<100)
+            {
+                i++;
+                if (i%2!=0)
+                    continue;
+                else
+                    Console.WriteLine(i);
+                if (i==50) break;
+            }
+        }
+    }
+}
---
+++ MultipleTable/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace MultipleTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //вывыод таблицы умножения с помощью всех трех типов циклов
+            //в каждом способе используются вложенные циклы
+
+            Console.WriteLine("таблица умножения через цикл с параметром");
+            //таблица умножения через цикл с параметром
+            for (int ii = 2; ii <10; ii++)
+            {
+                for (int jj = 1; jj<10; jj++)
+                {
+                    Console.WriteLine($"{ii} * {jj} = {ii*jj}");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("таблица умножения через цикл с предусловием") ;
+            //таблица умножения через цикл с предусловием
+            int i = 1;
+            while (i < 10)
+            {
+                int j = 1;
+                while (j < 10)
+                {
+                    Console.WriteLine($"{i} * {j} = {i*j}");
+                    j++;
+                }
+                i++;
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("таблица умножения через цикл с постусловием");
+            //таблица умножения через цикл с постусловием
+            i = 1;
+            do
+            {
+                int j = 1;
+                do
+                {
+                    Console.WriteLine($"{i} * {j} = {i*j}");
+                    j++;
+                }
+                while (j<10);
+                Console.WriteLine();
+                i++;
+            }
+            while (i<10);
+        }
+    }
+}
